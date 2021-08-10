f6e98836d014fc174cc7ede2480451bbc2a165c2
Attila B. Gál <galaty2@gmail.com>
2021-08-10 04:13:45 +0200

    TS webpack base

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import './index.less';
+
+import lol from './lol.png';
+
+console.log("hello from ts")
+console.log("lol href: ", lol)
+
+
+window.onload = (e) => {
+   console.log("window onload")
+}
